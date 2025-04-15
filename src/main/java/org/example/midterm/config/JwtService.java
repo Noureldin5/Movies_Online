421796ead8b4072a8df5b29ec48da6ff7d6a62e0
Noureldin5 <ns9121212@gmail.com>
2025-04-15 19:16:56 +0600

    fixed dependancy issue

--- src/main/java/org/example/midterm/config/JwtService.java
+++ src/main/java/org/example/midterm/config/JwtService.java
@@ -5,10 +5,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.example.midterm.entities.User;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
-
+@Service
 public class JwtService {
     private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15; // 15 minutes
